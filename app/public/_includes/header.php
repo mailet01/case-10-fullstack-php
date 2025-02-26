1eef7838d7c0df6ea0eb40825041912a25a0fac8
mailet01 <111491293+mailet01@users.noreply.github.com>
2025-02-26 15:10:36 +0100

    login or out

--- app/public/_includes/header.php
+++ app/public/_includes/header.php
@@ -5,8 +5,23 @@ emils bloggapplikation
 </header>
 <nav>
 <a href="index.php">startsida</a>    
-    <a href="login.php">Logga in</a> | 
+<?php 
+if(isset($_SESSION['user_id'])) {
+
+?>
+
     <a href="logout.php">Logga ut</a> | 
+<?php 
+} else {
+?>
+
+    <a href="login.php">Logga in</a> | 
+
+
+    ?>
+<?php
+}
+?>
     <a href="register.php">Registrera</a> | 
 <a href="page.php">skapa en sida</a> |
 <a href="page_edit.php">redigera sida</a>
